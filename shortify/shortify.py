59663f94055e0625a998f9c2a98d43ec8516ff96
Kotyara <krivonos-artem-alexandrovich@yandex.ru>
2017-12-23 12:55:45 +0300

    feat(shotify.py): add web.View

--- shortify/shortify.py
+++ shortify/shortify.py
@@ -9,14 +9,14 @@ REDIS_HOST = 'redis'
 
 
 class RedisBasedKeyValueStorage:
-    def __init__(self, host):
+    def __init__(self, host, ttl=60):
         self._client = StrictRedis(host=host, port=6379, db=0)
+        self._ttl = ttl
 
-    async def set(self, key, value, ttl=None):
+    async def set(self, key, value):
         key = 'urls:' + key
         await self._client.set(key, value)
-        if ttl is not None:
-            await self._client.expire(key, ttl)
+        await self._client.expire(key, self._ttl)
 
     async def get(self, key):
         return await self._client.get('urls:' + key)
@@ -27,25 +27,21 @@ def is_valid_url(url):
     return bool(parsed_url.scheme and parsed_url.netloc)
 
 
-class ShorterView:
-    def __init__(self, storage, ttl=60):
-        self._storage = storage
-        self._ttl = ttl
-
-    async def get_url(self, request):
-        hash_ = request.match_info.get('hash')
+class ShorterView(web.View):
+    async def get(self):
+        hash_ = self.request.match_info.get('hash')
         if hash_ is None:
             return web.Response(text=MAIN_PAGE, content_type='text/html')
-        url = await self._storage.get(hash_)
+        url = await self.request.app.storage.get(hash_)
         return web.HTTPFound(url.decode())
 
-    async def post_url(self, request):
-        data = await request.json()
+    async def post(self):
+        data = await self.request.json()
         url = data['url']
         if not is_valid_url(url):
             return web.json_response(status=400)
         hash_ = hex(binascii.crc32(url.encode()))[2:]
-        await self._storage.set(hash_, url, ttl=self._ttl)
+        await self.request.app.storage.set(hash_, url)
         return web.json_response(
             dict(path='/{}'.format(hash_))
         )
@@ -53,11 +49,11 @@ class ShorterView:
 
 def create_app(host):
     storage = RedisBasedKeyValueStorage(host)
-    view = ShorterView(storage)
     app = web.Application()
-    app.router.add_get('/{hash}', view.get_url)
-    app.router.add_get('/', view.get_url)
-    app.router.add_post('/', view.post_url)
+    app.storage = storage
+    app.router.add_get('/{hash}', ShorterView)
+    app.router.add_get('/', ShorterView)
+    app.router.add_post('/', ShorterView)
     return app
 
 
@@ -69,5 +65,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# TODO views
 # TODO monitoring
